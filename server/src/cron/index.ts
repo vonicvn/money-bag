7aad7b872db63f817ba904c50d2a4f681d31c1e7
Pho Nguyen <pho.nguyenvan@nashtechglobal.com>
2020-05-10 00:56:00 +0700

    Register functions to cronjob

---
+++ server/src/cron/index.ts
@@ -0,0 +1,30 @@
+/* istanbul ignore file */
+import { FactoryContract } from '../global'
+import { CronJob, CronCommand } from 'cron'
+import { map } from 'lodash'
+import { EthereumFactoryContract } from './ethereum-factory-contract'
+import { DepositContractScanner } from './deposit-contract-scanner'
+import { TransactionScanner } from './transaction-scanner'
+
+function createJob(time: string, cb: CronCommand, runOnInit = false) {
+  new CronJob(time, cb, null, true, null, undefined, runOnInit).start()
+}
+
+enum ECronTime {
+  EVERY_ONE_SECOND = '*/1 * * * * *',
+  EVERY_TWO_SECOND = '*/1 * * * * *',
+  EVERY_FIFTEEN_SECONDS = '*/15 * * * * *',
+  EVERY_ONE_MINUTE = '0 */1 * * * *',
+  EVERY_THREE_MINUTES = '0 */3 * * * *',
+  EVERY_FIVE_MINUTES = '0 */5 * * * *',
+  EVERY_TEN_MINUTES = '0 */10 * * * *',
+}
+
+export async function registerCronJobs() {
+  const factoryContracts = await FactoryContract.findAll({ status: 'ENABLED' })
+  const ethereumFactoryContracts = map(factoryContracts, contract => new EthereumFactoryContract(contract))
+  const depositContractScanner = new DepositContractScanner(ethereumFactoryContracts)
+  const transactionScannder = new TransactionScanner(ethereumFactoryContracts)
+  createJob(ECronTime.EVERY_TEN_MINUTES, () => depositContractScanner.process(), true)
+  createJob(ECronTime.EVERY_ONE_MINUTE, () => transactionScannder.process(), true)
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,13 @@
 /* istanbul ignore file */
 import { Env, EEnvKey, runMigrations, RouteLoader, RouteFinder } from './global'
 import { app } from './app'
+import { registerCronJobs } from './cron'
 
 async function start() {
   await runMigrations()
   await RouteLoader.load(app, await RouteFinder.find())
   app.listen(Env.get(EEnvKey.PORT), () => console.log('Server started.'))
+  await registerCronJobs()
 }
 
 start()
