4c1f7a612c2af5c4918842984742013dc5738484
Pho Nguyen <pho.nguyen@vn.woowahan.com>
2020-07-12 14:10:37 +0700

    Set default status to WAITING

--- database/migrations/20200418160903_addTransactionTable.ts
+++ database/migrations/20200418160903_addTransactionTable.ts
@@ -6,7 +6,10 @@ export async function up(knex: Knex): Promise<void> {
     addPrimaryKey(table, 'transaction_id')
     addCascadeForeignKey(table, 'partner', {})
     addCascadeForeignKey(table, 'asset', {})
-    table.enum('collecting_status', ['WAITING', 'PROCESSING', 'SUCCESS'])
+    table
+      .enum('collecting_status', ['WAITING', 'PROCESSING', 'SUCCESS'])
+      .defaultTo('WAITING')
+      .notNullable()
     table.string('hash', 256)
     table.string('asset_address', 256)
     table.integer('block', 10).defaultTo(0)
