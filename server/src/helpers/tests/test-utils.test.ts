83bbf03a597d57651e6c88161cdfd2f0b1b0454c
Pho Nguyen <pho.nguyen@vn.woowahan.com>
2020-07-23 22:24:16 +0700

    Change name to money bag

--- server/src/helpers/tests/test-utils.test.ts
+++ server/src/helpers/tests/test-utils.test.ts
@@ -6,12 +6,12 @@ const TEST_TITLE = TestUtils.getTestTitle(__filename)
 describe(TEST_TITLE, () => {
   it(`${TEST_TITLE} JTestUtilsWT works`, async () => {
     equal(
-      TestUtils.getTestTitle('C:/Working/ethereum_api/server/dist/helpers/tests/test-utils.test.js'),
+      TestUtils.getTestTitle('C:/Working/money_bag/server/dist/helpers/tests/test-utils.test.js'),
       '/helpers/tests/test-utils'
     )
 
     equal(
-      TestUtils.getTestTitle('C:/Working/ethereum_api/server/src/helpers/tests/test-utils.test.ts'),
+      TestUtils.getTestTitle('C:/Working/money_bag/server/src/helpers/tests/test-utils.test.ts'),
       '/helpers/tests/test-utils'
     )
   })
