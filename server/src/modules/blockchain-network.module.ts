c02f831ffdab360f84b4524d119facfb3aab2b95
Pho Nguyen <pho.nguyen@vn.woowahan.com>
2020-10-14 22:45:59 +0700

    Add getTransactionStatus

--- server/src/modules/blockchain-network.module.ts
+++ server/src/modules/blockchain-network.module.ts
@@ -1,10 +1,19 @@
 import { EBlockchainNetwork } from '../global'
 
+export enum EBlockchainTransactionStatus {
+  PENDING = 'PENDING',
+  WAIT_FOR_MORE_COMFIRMATIONS = 'WAIT_FOR_MORE_COMFIRMATIONS',
+  FAILED = 'FAILED',
+  SUCCESS = 'SUCCESS',
+}
+
 export interface IBlockchainNetwork {
   getBlockNumber(): Promise<number>
 
   // tslint:disable-next-line: no-any
   getTransactions(block: number): Promise<ITransactionInput[]>
+
+  getTransactionStatus(hash: string): Promise<EBlockchainTransactionStatus>
 }
 
 export interface ITransactionInput {
--- server/src/modules/ethereum-network/index.ts
+++ server/src/modules/ethereum-network/index.ts
@@ -1,5 +1,6 @@
-import { Web3InstanceManager } from '../../global'
-import { IBlockchainNetwork } from '../blockchain-network.module'
+import { isNil } from 'lodash'
+import { Env, Web3InstanceManager } from '../../global'
+import { EBlockchainTransactionStatus, IBlockchainNetwork } from '../blockchain-network.module'
 import { TransactionsGetter } from './transaction-getter'
 
 export class EthereumNetwork implements IBlockchainNetwork {
@@ -9,5 +10,17 @@ export class EthereumNetwork implements IBlockchainNetwork {
 
   getTransactions(block: number) {
     return new TransactionsGetter(block).get()
+  }
+
+  async getTransactionStatus(hash: string) {
+    const receipt = await Web3InstanceManager.defaultWeb3.eth.getTransactionReceipt(hash)
+    if (isNil(receipt)) return EBlockchainTransactionStatus.PENDING
+    if (receipt.status) {
+      const currentBlock = await Web3InstanceManager.defaultWeb3.eth.getBlockNumber()
+      const shouldWaitForMoreConfirmations = currentBlock - receipt.blockNumber < Env.SAFE_NUMBER_OF_COMFIRMATION
+      if (shouldWaitForMoreConfirmations) return EBlockchainTransactionStatus.WAIT_FOR_MORE_COMFIRMATIONS
+      return EBlockchainTransactionStatus.SUCCESS
+    }
+    return EBlockchainTransactionStatus.FAILED
   }
 }
