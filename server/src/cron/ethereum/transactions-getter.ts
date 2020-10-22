ea7d22d86769e4cb77301cd750ff970280e40e94
Pho Nguyen <pho.nguyen@vn.woowahan.com>
2020-10-23 01:44:55 +0700

    check the log

--- server/src/cron/ethereum/transactions-getter.ts
+++ server/src/cron/ethereum/transactions-getter.ts
@@ -31,6 +31,7 @@ export class TransactionsGetter {
   async get() {
     const results = []
     const ethereumTransactions = await this.getBlock()
+    console.log(ethereumTransactions)
     for (const ethereumTransaction of ethereumTransactions) {
       results.push(await this.parseEthereumTransaction(ethereumTransaction))
     }
