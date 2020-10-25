0d1b4562e2ba7b855c5bfd2320f012897be47f8f
Pho Nguyen <pho.nguyen@vn.woowahan.com>
2020-10-25 23:46:06 +0700

    Scan tron faster

--- server/src/cron/index.ts
+++ server/src/cron/index.ts
@@ -7,13 +7,11 @@ function createJob(time: string, cb: CronCommand, runOnInit = false) {
   new CronJob(time, cb, null, true, null, undefined, runOnInit).start()
 }
 
-const EVERY_FIFTEEN_SECONDS = '17 * * * * *'
-
 export async function registerCronJobs() {
   if (Env.get(EEnvKey.STOP_SCAN) === 'true') return
   const ethereumScanner = new EthereumScanner(EBlockchainNetwork.ETHEREUM)
-  createJob(EVERY_FIFTEEN_SECONDS, () => ethereumScanner.process(), true)
+  createJob('17 * * * * *', () => ethereumScanner.process(), true)
 
   const tronScanner = new EthereumScanner(EBlockchainNetwork.TRON)
-  createJob(EVERY_FIFTEEN_SECONDS, () => tronScanner.process(), true)
+  createJob('/10 * * * * *', () => tronScanner.process(), true)
 }
