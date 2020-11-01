a84f9455d5553bbe1dfa388fa6fce4d61b7435d6
Pho Nguyen <pho.nguyen@vn.woowahan.com>
2020-11-01 23:03:45 +0700

    Done

--- server/src/modules/blockchain/tron-network/trc20-token.ts
+++ server/src/modules/blockchain/tron-network/trc20-token.ts
@@ -10,7 +10,6 @@ export class Trc20Token {
   }
 
   private async getSpenderContract() {
-    console.log({ spenderAdmin: this.privateKey })
     const tronWeb = await TronWebInstance.getByPrivateKey(this.privateKey)
     return tronWeb.contract().at(Env.get(EEnvKey.TRON_SPENDER_CONTRACT_ADDRESS))
   }
@@ -21,7 +20,6 @@ export class Trc20Token {
       walletAddress,
       Env.get(EEnvKey.TRON_SPENDER_CONTRACT_ADDRESS)
     ).call()
-    console.log({ remaining: remaining.toNumber() })
     return remaining.toNumber() !== 0
   }
 
@@ -44,7 +42,7 @@ export class Trc20Token {
         to,
         value
       )
-      .send({  })
+      .send({})
     return txID
   }
 
