9dcd2c0a5eebdbfb532e1c52a98a49019aacfaa4
Pho Nguyen <pho.nguyen@vn.woowahan.com>
2020-11-11 22:30:15 +0700

    Fix bug create withdrawal

--- server/src/apis/v1/withdrawals/create/route.ts
+++ server/src/apis/v1/withdrawals/create/route.ts
@@ -1,10 +1,10 @@
 import { body } from 'express-validator'
-import { EMethod, createService, OnlyAdminRoute } from '../../../shared'
+import { EMethod, createService, OnlyPartnerRoute } from '../../../shared'
 import { ApiExcutor } from './api-excutor'
 import { InputGetter } from './input-getter'
 import { InputValidator } from './input-validator'
 
-export class Route extends OnlyAdminRoute {
+export class Route extends OnlyPartnerRoute {
   path = '/api/v1/withdrawals'
   method = EMethod.POST
   Service = createService(InputGetter, InputValidator, ApiExcutor)
