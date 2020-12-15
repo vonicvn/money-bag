fe8af00d5128efaa67eadf7c92c30fca3cd40be0
Pho Nguyen <pho.nguyen@vn.woowahan.com>
2020-12-15 16:09:14 +0700

    Update start message

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,7 +8,7 @@ async function start() {
   await new AppBootstrap().bootstrap()
   await RouteLoader.load(app, await RouteFinder.find())
   const PORT = Env.get(EEnvKey.PORT)
-  app.listen(PORT, () => console.log('Server started at port '.concat(PORT)))
+  app.listen(PORT, () => console.log('Server started at port:: '.concat(PORT)))
   await registerCronJobs()
 }
 
